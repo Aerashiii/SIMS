0b73615330ff8d850134a9e31dbea017497b487b
BenjarBulacon <benjarbulacon09@gmail.com>
2024-11-25 21:38:30 +0800

    update dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,6 +1,5 @@
  <!-- | ALL DASHBOARD CONTENT ONLY HERE |-->
  
-
 <?php 
     $page = 'dashboard'; //ASSIGNS THE NAME OF THE PAGE. THIS PAGE IS NAMED 'DASHBOARD' AND IS USED IN THE HEADER.PHP FILE.
     include '../includes/header.php'; //REQUIRES THE HEADER.PHP, FOR YOU CAN APPLY THE CSS, SCRIPT, SIDEBAR, AND TOPNAV ON THIS PAGE.
@@ -10,6 +9,95 @@
 <main class="main-content-container"><!-- | THE STYLES FOR THIS CONTAINER ARE DEFINED IN GLOBAL.CSS TO STANDARDIZE THE STYLE OF THE MAIN CONTAINER ACROSS ALL PAGES |-->
     <!-- | your code here |-->
     <h1>Dashboard</h1>
+    <h2>Top Metrics</h2>
+
+    <div class="top-metrics-box-container">
+        <div class="total-sales-container">
+            <span id="total-sales">0</span>
+            <h3>Total Sales</h3>
+
+        </div>
+        <div class="inventory-value-container">
+            <span id="inventory-value">0</span>
+            <h3>Total Sales</h3>
+
+        </div>
+        <div class="low-stocks-items-container">
+            <span id="low-stock-items">1000</span>
+            <h3>Total Sales</h3>
+
+        </div>
+        <div class="occupied-rental-boxes-container">
+            <span id="occupied-rentas-boxes">0</span>
+            <h3>Total Sales</h3>
+
+        </div>
+
+    </div>
+
+    <div class="sales-details-container">
+        <div class="recent-sales-container">
+            <h2>Recent Sales</h2>
+            <table id="recent-sales-table">
+                <tr>
+                    <th>Date</th>
+                    <th>Product</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
+                </tr>
+                <!-- |SAMPLE|-->
+                <tr>
+                    <td>December 10 2024</td>
+                    <td>Speaker</td>
+                    <td>5</td>
+                    <td>1500</td>
+
+                </tr>
+            </table>
+
+        </div>
+        <div class="low-stock-alert-container">
+        <h2>LOw Stock Alert</h2>
+            <table id="low-stock-alert-table">
+                <tr>
+                    <th>Product Name</th>
+                    <th>Current Stock Level</th>
+                    <th>Alert</th>
+                  
+                </tr>
+                <!-- |SAMPLE|-->
+                <tr>
+                   
+                    <td>Speaker</td>
+                    <td>5</td>
+                    <td>1500</td>
+
+                </tr>
+            </table>
+
+        </div>
+        <div class="rental-box-summary-container">
+        <h2>Rental Box Summary</h2>
+            <table id="rental-box-summary-table">
+                <tr>
+                    <th>Box Number</th>
+                    <th>Renter</th>
+                    <th>Status</th>
+                    <th>Date</th>
+                </tr>
+                <!-- |SAMPLE|-->
+                <tr>
+                    <td>2024</td>
+                    <td>Speaker</td>
+                    <td>5</td>
+                    <td>December 10 2024</td>
+
+                </tr>
+            </table>
+
+        </div>
+
+    </div>
 
 
 </main>
--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,28 @@
 <?php
     $page="login";
+    include '../includes/header.php';
 ?>
+
+<div class="login-form-main-container">
+    <div class="form-container">
+        <form action="" id="login-form">
+            <h2>General's Space Rent</h2>
+            <p>A Web-based Sales and Inventory Management System</p>
+            <h4>Hey, Hello! <img src="../assets/images/icons/wave.png" alt=""></h4>
+            <div class="login-form-input-contaner">
+                <label for="">Entry Type:</label>
+                <select name="" id="entry-type" name="entry-type">
+                    <option value="">Admin</option>
+                </select>
+                <label for="">Username:</label>
+                <input type="text" id="username" name="username">
+                <label for="">Password:</label>
+                <input type="password" id="password" name="password">
+                
+                <input type="submit" name="submit" id="login-submit-button" value="Login">
+            </div>
+
+        </form>
+    </div>
+
+</div>
