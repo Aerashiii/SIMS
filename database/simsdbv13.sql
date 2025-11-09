2415c7e3da32d8bf2f40f58ddd59bcc9532a2bb6
Aira <146685592+BenjarBulacon@users.noreply.github.com>
2025-11-09 20:37:39 +0800

    update

---
+++ database/simsdbv13.sql
@@ -0,0 +1,534 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 09, 2025 at 01:36 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `simsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brand`
+--
+
+CREATE TABLE `brand` (
+  `brand_id` int(11) NOT NULL,
+  `brand_name` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `deleted` varchar(6) NOT NULL,
+  `date_created` datetime(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business_details`
+--
+
+CREATE TABLE `business_details` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `logo` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `contact_number` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `daily_rate` varchar(255) NOT NULL,
+  `weekly_rate` varchar(255) NOT NULL,
+  `monthly_rate` varchar(255) NOT NULL,
+  `low_strock_alert` varchar(255) NOT NULL,
+  `overdue_rental_alert` varchar(255) NOT NULL,
+  `date_create` datetime(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `deleted` varchar(6) NOT NULL,
+  `date_created` datetime(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ordered_products`
+--
+
+CREATE TABLE `ordered_products` (
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `barcode` varchar(255) NOT NULL,
+  `brand_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `subcategory_id` int(11) DEFAULT NULL,
+  `original_price` int(11) NOT NULL,
+  `selling_price` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `reorder_point` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `supplier_id` int(11) DEFAULT NULL,
+  `description` varchar(255) NOT NULL,
+  `date_created` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `deleted` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_sales`
+--
+
+CREATE TABLE `product_sales` (
+  `transact_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity_sold` int(11) NOT NULL,
+  `total_sale` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_order_transaction`
+--
+
+CREATE TABLE `purchase_order_transaction` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `total_product` int(11) NOT NULL,
+  `total_cost` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `date` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `deleted` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rental_box`
+--
+
+CREATE TABLE `rental_box` (
+  `box_id` int(11) NOT NULL,
+  `box_number` int(11) NOT NULL,
+  `box_size` varchar(20) NOT NULL,
+  `width` int(11) NOT NULL,
+  `length` int(11) NOT NULL,
+  `rental_fee` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `date_created` datetime(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rental_transaction`
+--
+
+CREATE TABLE `rental_transaction` (
+  `id` int(11) NOT NULL,
+  `renter_id` int(11) NOT NULL,
+  `rented_quantity` int(11) NOT NULL,
+  `payment` int(11) NOT NULL,
+  `rental_start_date` date NOT NULL,
+  `rental_end_date` date NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `deleted` varchar(6) NOT NULL,
+  `date_created` datetime(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rented_box_transaction`
+--
+
+CREATE TABLE `rented_box_transaction` (
+  `id` int(11) NOT NULL,
+  `renter_id` int(11) NOT NULL,
+  `rental_transaction_id` int(11) NOT NULL,
+  `box_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `date_created` datetime(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `renter`
+--
+
+CREATE TABLE `renter` (
+  `renter_id` int(11) NOT NULL,
+  `renter_name` varchar(50) NOT NULL,
+  `contact_number` varchar(20) NOT NULL,
+  `date_create` datetime(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales_transaction`
+--
+
+CREATE TABLE `sales_transaction` (
+  `transact_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `total_items` int(11) NOT NULL,
+  `total_payment` int(11) NOT NULL,
+  `cash` int(11) NOT NULL,
+  `change_amt` int(11) NOT NULL,
+  `payment_method` varchar(20) NOT NULL,
+  `customer_name` varchar(100) NOT NULL,
+  `contact_number` varchar(255) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subcategory`
+--
+
+CREATE TABLE `subcategory` (
+  `subcategory_id` int(11) NOT NULL,
+  `subcategory_name` varchar(255) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `deleted` varchar(6) NOT NULL,
+  `date_created` datetime(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `supplier_id` int(11) NOT NULL,
+  `supplier_name` varchar(255) DEFAULT NULL,
+  `contact_person` varchar(255) DEFAULT NULL,
+  `contact_number` varchar(20) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `supplier_type` varchar(255) DEFAULT NULL,
+  `product_category_id` int(11) DEFAULT NULL,
+  `payment_terms` varchar(255) DEFAULT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  `deleted` varchar(6) DEFAULT NULL,
+  `date_added` datetime(6) DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `profile_pic` varchar(255) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `username`, `password`, `role`, `profile_pic`, `date_created`) VALUES
+(1, 'admin', 'admin', 'admin', 'admin', '../../assets/images/uploads/68d895b3e6a98_default-profile.png', '2025-11-09 12:35:20');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brand`
+--
+ALTER TABLE `brand`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indexes for table `business_details`
+--
+ALTER TABLE `business_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `ordered_products`
+--
+ALTER TABLE `ordered_products`
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `brand_id` (`brand_id`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `subcategory_id` (`subcategory_id`),
+  ADD KEY `supplier_id` (`supplier_id`);
+
+--
+-- Indexes for table `product_sales`
+--
+ALTER TABLE `product_sales`
+  ADD PRIMARY KEY (`transact_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `purchase_order_transaction`
+--
+ALTER TABLE `purchase_order_transaction`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier_id` (`supplier_id`);
+
+--
+-- Indexes for table `rental_box`
+--
+ALTER TABLE `rental_box`
+  ADD PRIMARY KEY (`box_id`);
+
+--
+-- Indexes for table `rental_transaction`
+--
+ALTER TABLE `rental_transaction`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `renter_id` (`renter_id`);
+
+--
+-- Indexes for table `rented_box_transaction`
+--
+ALTER TABLE `rented_box_transaction`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `renter_id` (`renter_id`),
+  ADD KEY `rental_transaction_id` (`rental_transaction_id`),
+  ADD KEY `box_id` (`box_id`);
+
+--
+-- Indexes for table `renter`
+--
+ALTER TABLE `renter`
+  ADD PRIMARY KEY (`renter_id`);
+
+--
+-- Indexes for table `sales_transaction`
+--
+ALTER TABLE `sales_transaction`
+  ADD PRIMARY KEY (`transact_id`);
+
+--
+-- Indexes for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  ADD PRIMARY KEY (`subcategory_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`supplier_id`),
+  ADD KEY `product_category_id` (`product_category_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brand`
+--
+ALTER TABLE `brand`
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `business_details`
+--
+ALTER TABLE `business_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product_sales`
+--
+ALTER TABLE `product_sales`
+  MODIFY `transact_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `purchase_order_transaction`
+--
+ALTER TABLE `purchase_order_transaction`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rental_box`
+--
+ALTER TABLE `rental_box`
+  MODIFY `box_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rental_transaction`
+--
+ALTER TABLE `rental_transaction`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rented_box_transaction`
+--
+ALTER TABLE `rented_box_transaction`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `renter`
+--
+ALTER TABLE `renter`
+  MODIFY `renter_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sales_transaction`
+--
+ALTER TABLE `sales_transaction`
+  MODIFY `transact_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  MODIFY `subcategory_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `supplier_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ordered_products`
+--
+ALTER TABLE `ordered_products`
+  ADD CONSTRAINT `ordered_products_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`brand_id`) REFERENCES `brand` (`brand_id`),
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
+  ADD CONSTRAINT `products_ibfk_3` FOREIGN KEY (`subcategory_id`) REFERENCES `subcategory` (`subcategory_id`),
+  ADD CONSTRAINT `products_ibfk_4` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`supplier_id`);
+
+--
+-- Constraints for table `product_sales`
+--
+ALTER TABLE `product_sales`
+  ADD CONSTRAINT `product_sales_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `purchase_order_transaction`
+--
+ALTER TABLE `purchase_order_transaction`
+  ADD CONSTRAINT `purchase_order_transaction_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`supplier_id`);
+
+--
+-- Constraints for table `rental_transaction`
+--
+ALTER TABLE `rental_transaction`
+  ADD CONSTRAINT `rental_transaction_ibfk_1` FOREIGN KEY (`renter_id`) REFERENCES `renter` (`renter_id`);
+
+--
+-- Constraints for table `rented_box_transaction`
+--
+ALTER TABLE `rented_box_transaction`
+  ADD CONSTRAINT `rented_box_transaction_ibfk_1` FOREIGN KEY (`renter_id`) REFERENCES `renter` (`renter_id`),
+  ADD CONSTRAINT `rented_box_transaction_ibfk_2` FOREIGN KEY (`rental_transaction_id`) REFERENCES `rental_transaction` (`id`),
+  ADD CONSTRAINT `rented_box_transaction_ibfk_3` FOREIGN KEY (`box_id`) REFERENCES `rental_box` (`box_id`);
+
+--
+-- Constraints for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  ADD CONSTRAINT `subcategory_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`);
+
+--
+-- Constraints for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD CONSTRAINT `supplier_ibfk_1` FOREIGN KEY (`product_category_id`) REFERENCES `category` (`category_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
